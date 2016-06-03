9b911012dc90d5145cc8623c157c09e7d3728d4d
Lucas Salgado <lucas.salgado@uniriotec.br>
2016-06-03 15:27:08 -0300

    Refeito utilizando bibliotecas aceitas

---
+++ src/acessorios/Descompactador.java
@@ -0,0 +1,45 @@
+package acessorios;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Descompactador {
+
+	final static int BUFFER = 2048;
+
+	public static void descompactar() {
+		
+		FileInputStream zipinho;
+		try {
+			String destino = null;
+			BufferedOutputStream dest = null;
+			zipinho = new FileInputStream("1.zip");
+			ZipInputStream zipOrigem = new ZipInputStream(
+					new BufferedInputStream(zipinho));
+			ZipEntry entry;
+			while ((entry = zipOrigem.getNextEntry()) != null) {
+				int count;
+				byte data[] = new byte[BUFFER];
+				// write the files to the disk
+				destino = "curriculos/" + entry.getName();
+				FileOutputStream fos = new FileOutputStream(destino);
+				dest = new BufferedOutputStream(fos, BUFFER);
+				while ((count = zipOrigem.read(data, 0, BUFFER)) != -1) {
+					dest.write(data, 0, count);
+				}
+				dest.flush();
+				dest.close();
+			}
+			zipOrigem.close();
+			System.out.println("Conteúdo descompactado para : " +destino);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
