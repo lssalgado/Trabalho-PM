95692769ae2526574212fe03446873b4eeaca30a
Lucas Salgado <lucas.salgado@uniriotec.br>
2016-06-14 15:33:13 -0300

    Remodelagem do programa

---
+++ src/model/Curriculo.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.List;
+
+public class Curriculo {
+
+	private List<Artigos> artigoPeriodicoA1;
+	private List<Artigos> artigoPeriodicoA2;
+	private List<Artigos> artigoPeriodicoB1;
+	private List<Artigos> artigoPeriodicoB2;
+	private List<Artigos> artigoPeriodicoB3;
+	private List<Artigos> artigoPeriodicoB4;
+	private List<Artigos> artigoPeriodicooC;
+	private List<Artigos> artigoPeriodicoNC;
+	private List<Artigos> artigoConferenciaA1;
+	private List<Artigos> artigoConferenciaA2;
+	private List<Artigos> artigoConferenciaB1;
+	private List<Artigos> artigoConferenciaB2;
+	private List<Artigos> artigoConferenciaB3;
+	private List<Artigos> artigoConferenciaB4;
+	private List<Artigos> artigoConferenciaC;
+	private List<Artigos> artigoConferenciaNC;
+	private List<Banca> bancasDoutorado;
+	private List<Banca> bancasMestrado;
+	private List<Banca> bancasGraduacao;
+	private List<Orientacao> orientaçoesDoutoradoConcluidas;
+	private List<Orientacao> orientaçoesMestradoConcluidas;
+	private List<Orientacao> orientaçoesProjetoFinalConcluidas;
+	private List<Orientacao> orientaçoesDoutoradoAndamento;
+	private List<Orientacao> orientaçoesMestradoAndamento;
+	private List<Orientacao> orientaçoesProjetoFinalAndamento;
+	
+	
+	
+}
